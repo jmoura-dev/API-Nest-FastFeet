fbd580be86ff007e9cea236d7e0e04c84c2c6987
Jackson <jackson1712@hotmail.com>
2023-11-07 13:50:39 -0300

    feat: create middleware to control permissions in routes

---
+++ src/middlewares/verify-is-admin-middleware.ts
@@ -0,0 +1,32 @@
+import {
+  Injectable,
+  NestMiddleware,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common'
+import { CurrentUser } from 'src/auth/current-user-decorator'
+import { UserPayload } from 'src/auth/jwt.strategy'
+import { PrismaService } from 'src/prisma/prisma.service'
+
+@Injectable()
+export class IsAdminMiddleware implements NestMiddleware {
+  constructor(private prisma: PrismaService) {}
+
+  async use(@CurrentUser() user: UserPayload, _: any, next: () => void) {
+    const loggedUser = await this.prisma.user.findUnique({
+      where: {
+        id: user.sub,
+      },
+    })
+
+    if (!loggedUser) {
+      throw new NotFoundException('Please log in again.')
+    }
+
+    if (loggedUser.role === 'DELIVERYMAN') {
+      throw new UnauthorizedException('You are not allowed.')
+    }
+
+    next()
+  }
+}
